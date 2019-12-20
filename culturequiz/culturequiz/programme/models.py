6ef6e1fa8cda91bcc976e61f63fce3b68a11a59e
purerstamp <edouard@the-redunicorn.fr>
2019-12-20 14:37:13 +0100

    Correcting programme.models (id not auto)

--- culturequiz/culturequiz/programme/models.py
+++ culturequiz/culturequiz/programme/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Bar(models.Model):
+  id = models.AutoField(primary_key=True)
   name = models.CharField(max_length=50)
   address = models.CharField(max_length=200)
   tel = models.CharField(max_length=10)
@@ -10,9 +11,17 @@ class Bar(models.Model):
   openingTime2 = models.CharField(max_length=5)
   closingTime2 = models.CharField(max_length=5)
   website = models.CharField(max_length=200)
+  def __str__(self):
+    return self.name
 
 class Event(models.Model):
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=200)
   type = models.CharField(max_length=50)
   day = models.CharField(max_length=8)
   time = models.CharField(max_length=5)
   location = models.ForeignKey(Bar, on_delete=models.DO_NOTHING)
+  def __str__(self):
+    return self.name
+  def allByDay(self, day):
+    return self.day == "day"
