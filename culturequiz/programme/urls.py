96449c2fcb75d4dca527bbff42f9b18391981b39
purerstamp <edouard@the-redunicorn.fr>
2020-01-02 13:29:45 +0100

    Trying to understand where to put this f*cking code

--- culturequiz/programme/urls.py
+++ culturequiz/programme/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
+from programme.views import EventList
 
 urlpatterns = [
     path('', EventList.as_view()),
-    path('programme/<str:day>/', views.potd, name='potd'),
+    #path('programme/<str:day>/', views.potd, name='potd'),
 ]
 
