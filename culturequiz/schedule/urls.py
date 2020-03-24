4a4ed9d9032131c8ae32bb320b6c6a8501b218be
purerstamp <edouard@the-redunicorn.fr>
2020-03-24 18:46:44 +0100

    Added 'by day' events displaying with more views. Cleared pattern.

--- culturequiz/schedule/urls.py
+++ culturequiz/schedule/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
-from schedule.views import EventList
 
 urlpatterns = [
-    path('', EventList.as_view()),
-    #path('programme/<str:day>/', views.potd, name='potd'),
+    path('', views.EventList.as_view(), name='indexSchedule'),
+    path('schedule/0/', views.MonEventsList.as_view(), name='monEvents'),
+    path('schedule/1/', views.TueEventsList.as_view(), name='tueEvents'),
+    path('schedule/2/', views.WedEventsList.as_view(), name='wedEvents'),
+    path('schedule/3/', views.ThuEventsList.as_view(), name='thuEvents'),
+    path('schedule/4/', views.FriEventsList.as_view(), name='friEvents'),
+    path('schedule/5/', views.SatEventsList.as_view(), name='satEvents'),
+    path('schedule/6/', views.SunEventsList.as_view(), name='sunEvents'),
+    path('schedule/event/<int:pk>', views.EventDetail.as_view(), name='eventDetail'),
 ]
-
--- culturequiz/schedule/views.py
+++ culturequiz/schedule/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from datetime import datetime
 from schedule.models import Event
 
 class EventList(ListView):
+  """
+    This view is part of the schedule app.
+    Return all events and all events by day of week. Use this view for the main index.
+  """
   context_object_name = 'event_list'
   template_name = 'schedule/event_list.html'
   queryset = Event.objects.all()
@@ -19,10 +24,95 @@ class EventList(ListView):
     context['event_sun_list'] = self.queryset.filter(day__iexact='dimanche')
     return context
 
-# Create your views here.
-#def index(request):
-#    return HttpResponse("Ici, retrouvez le programme de la semaine.")
-#
-# Program of the day
-#def potd(request, day):
-#   return HttpResponse("Programme de %s" % day)
+class MonEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Return all Monday's events. Use this view to build a day-specific page.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='lundi')
+  def get_context_data(self, **kwargs):
+    context = super(MonEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Lundi"
+    return context
+
+class TueEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Same as MonEventsList, for Tuesday.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='mardi')
+  def get_context_data(self, **kwargs):
+    context = super(TueEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Mardi"
+    return context
+
+class WedEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Same as MonEventsList, for Wednesday.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='mercredi')
+  def get_context_data(self, **kwargs):
+    context = super(WedEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Mercredi"
+    return context
+
+class ThuEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Same as MonEventsList, for Thursday.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='jeudi')
+  def get_context_data(self, **kwargs):
+    context = super(ThuEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Jeudi"
+    return context
+
+class FriEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Same as MonEventsList, for Friday.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='vendredi')
+  def get_context_data(self, **kwargs):
+    context = super(FriEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Vendredi"
+    return context
+
+class SatEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Same as MonEventsList, for Saturday.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='samedi')
+  def get_context_data(self, **kwargs):
+    context = super(SatEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Samedi"
+    return context
+
+class SunEventsList(ListView):
+  """
+    This view is part of the schedule app.
+    Same as MonEventsList, for Sunday.
+  """
+  template_name = 'schedule/byDay_events_list.html'
+  queryset = Event.objects.filter(day__iexact='dimanche')
+  def get_context_data(self, **kwargs):
+    context = super(SunEventsList, self).get_context_data(**kwargs)
+    context['dayOfWeek'] = "Dimanche"
+    return context
+
+class EventDetail(DetailView):
+  """
+    This view is part of the schedule app.
+    Displays details about a particular event.
+  """
+  template_name = 'schedule/event_detail.html'
+  model = Event
+  
